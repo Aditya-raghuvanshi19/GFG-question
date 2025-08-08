94ceb96e69fb63c3aa9c09af9355edccf6b42b39
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-08 19:01:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.java
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.java
@@ -1,51 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            int a;
-            a = Integer.parseInt(br.readLine());
-
-            int b;
-            b = Integer.parseInt(br.readLine());
-
-            Solution obj = new Solution();
-            int[] ans = obj.lcmAndGcd(a, b);
-            System.out.println(ans[0] + " " + ans[1]);
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
-
-// User function Template for Java
-
 class Solution {
     public static int gcd(int a,int b){
-        if(b==0)
-        return a;
-       
-        return gcd(b,a%b);
-        
+        if(a==0)return b;
+        return gcd(b%a , a);
     }
     public static int[] lcmAndGcd(int a, int b) {
-        int[] arr=new int[2];
-        arr[1]=gcd(a,b);
-        arr[0]=(a/arr[1])*b;// to avoid overflow first divide than multiply, if multiply first then divide
-        return arr;         //overflow occur
+       
+        int[] ans=new int[2];
+        int hcf=gcd(a,b);
+        ans[1]=hcf;
+        ans[0]=(a*b)/hcf;
+        return ans;
     }
 }
-
-//{ Driver Code Starts.
-
-// } Driver Code Ends
