af4526488e5eda8ea171d1f8a600be16e3905567
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-10 19:41:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,62 +1,36 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
-
 class Solution {
-    // Function to sort an array of 0s, 1s, and 2s
-    public void swap(int[]arr,int a,int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
     public void sort012(int[] arr) {
-        int low=0;
-        int mid=0;
-        int high=arr.length-1;
-        while(mid<=high){
-            if(arr[mid]==0){
-                swap(arr,low,mid);
-                low++;
-                mid++;
-            }else if(arr[mid]==1){
-                mid++;
-            }else{
-                swap(arr,mid,high);
-                high--;
+        
+// 0 range: 0..i-1 → [0, 0]
+
+// 1 range: i..j-1 → [1, 1]
+
+// Unprocessed: j..k → (empty now)
+
+// 2 range: k+1..n-1 → [2, 2]
+        int i=0;
+        int j=0;
+        int k=arr.length-1;
+        
+        while(j<=k){
+            if(arr[j]==0){
+                swap(arr,i,j);
+                i++;
+                j++;
+            
             }
+            else if(arr[j]==2){
+               swap(arr,j,k);
+               k--;
+               
+            }else
+            j++;
         }
         
     }
 }
-
-
-//{ Driver Code Starts.
-// } Driver Code Ends
