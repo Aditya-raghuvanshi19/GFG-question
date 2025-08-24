1443515a1f3a56bc0111b50b47984737456c5d61
Aditya Raghuvanshi <115358168+Aditya-raghuvanshi19@users.noreply.github.com>
2025-08-25 01:58:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.java
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -1,22 +1,28 @@
 // User function Template for Java
 
 class Solution {
+    public int findmin(int n,int[] arr,int[]dp,int k){
+        if(n==0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        
+        int mincost=Integer.MAX_VALUE;
+        for(int i=1;i<=k;i++){
+            if(n-i>=0){
+                int cost=findmin(n-i,arr,dp,k) + Math.abs(arr[n]-arr[n-i]);
+                mincost=Math.min(mincost,cost);
+            }
+            
+        }
+        dp[n]=mincost;
+        return dp[n];
+        
+    }
     public int minimizeCost(int k, int arr[]) {
         int n=arr.length;
         int[] dp=new int[n+1];
-        dp[0]=0;
+        Arrays.fill(dp,-1);
         
-        for(int i=1;i<n;i++){
-            int mincost=Integer.MAX_VALUE;
-            for(int j=1;j<=k;j++){
-                if((i-j)>=0){
-                int cost=dp[i-j] + Math.abs(arr[i]-arr[i-j]);
-                mincost=Math.min(mincost,cost);
-                }
-                
-            }
-            dp[i]=mincost;
-        }
-        return dp[n-1];
+        return findmin(n-1,arr,dp,k);
+        
     }
 }
